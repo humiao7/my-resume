5e3000d18fc19c93bda9ded73d2af78c27f7e8a3
Humiao <1124003166@qq.com>
2020-06-05 16:47:43 +0800

    初始化项目

---
+++ pages/home/home.js
@@ -0,0 +1,107 @@
+var app = getApp();
+Page({
+  /** 
+   * 页面的初始数据 
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    open: false
+  },
+
+  // 打开侧栏菜单
+  openSlider: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+    } else {
+      this.setData({
+        open: true
+      });
+    }
+  },
+
+  // touchstart事件
+  tap_start: function (e) {
+    // 把手指触摸屏幕的那一个点的 x 轴坐标赋值给 mark 和 newmark
+    this.data.mark = this.data.newmark = e.touches[0].pageX;
+  },
+
+  // touchmove事件
+  tap_drag: function (e) {
+    this.data.newmark = e.touches[0].pageX;
+    // 手指从左向右移动
+    if (this.data.mark < this.data.newmark) {
+      this.istoright = true;
+    }
+    // 手指从右向左移动
+    if (this.data.mark > this.data.newmark) {
+      this.istoright = false;
+    }
+    this.data.mark = this.data.newmark;
+  },
+
+  // touchend事件
+  tap_end: function (e) {
+    this.data.mark = 0;
+    this.data.newmark = 0;
+    // 通过改变 opne 的值，让主页加上滑动的样式
+    if (this.istoright) {
+      this.setData({
+        open: true
+      });
+    } else {
+      this.setData({
+        open: false
+      });
+    }
+  },
+
+  quit: function () {
+    app.globalData.userInfo = null;
+    this.setData({
+      userInfo: null,
+      hasUserInfo: false
+    })
+  },
+
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  // 获取用户信息
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+//index.js
+
+Page({
+  data: {
+    menuList: [{
+      name:"基础组件"
+    }]
+  }
+})
